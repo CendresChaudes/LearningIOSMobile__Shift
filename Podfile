9f728a38b3eb0c9c07d2717f9c7e706d80dfc8d1
anx1senwy <romqaaa1337@gmail.com>
2025-07-01 17:12:47 +0300

    [init] Create base of project

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '15.0'
+
+target 'Shift' do
+  use_frameworks!
+
+  pod 'SwiftLint', '0.59.1'
+
+end
---
+++ Shift/App/AppDelegate.swift
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.swift
+//  Shift
+//
+//  Created by Роман on 01.07.2025.
+//
+
+import CoreData
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Shift")
+
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+
+        return container
+    }()
+
+    // MARK: - UISceneSession Lifecycle
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        let rootViewController = TestViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    // MARK: - Core Data
+    func saveContext() {
+        let context = persistentContainer.viewContext
+
+        if     context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
