21849cb3db1b97b59504953056cec01da3f0600b
anx1senwy <romqaaa1337@gmail.com>
2025-07-06 17:25:56 +0300

    [test] Init tests

---
+++ ShiftTests/ShiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  ShiftTests.swift
+//  ShiftTests
+//
+//  Created by Роман on 06.07.2025.
+//
+
+import Testing
+
+struct ShiftTests {
+
+    @Test
+    func example() async {
+        #expect(1 == 1)
+    }
+}
